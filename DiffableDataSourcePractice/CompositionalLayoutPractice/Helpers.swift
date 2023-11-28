42122c6bd6c099331a947fc94e88dde5247d8d41
Amais Sheikh <amaissheikh@Amaiss-MacBook-Pro.local>
2023-11-28 18:43:11 +0500

    collection compositional layout practice

---
+++ DiffableDataSourcePractice/CompositionalLayoutPractice/Helpers.swift
@@ -0,0 +1,38 @@
+//
+//  Helpers.swift
+//  DiffableDataSourcePractice
+//
+//  Created by Amais Sheikh on 28/11/2023.
+//
+
+import UIKit
+
+class SampleCollectionCell: UICollectionViewCell {
+    
+    var imageView: UIImageView = {
+        let obj = UIImageView()
+        obj.translatesAutoresizingMaskIntoConstraints = false
+        obj.backgroundColor = .darkGray
+        return obj
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        contentView.addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+}
